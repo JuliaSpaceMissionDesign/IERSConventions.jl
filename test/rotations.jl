4862c14192baf61dd719eb528ff0497f6cb92c73
MicheleCeresoli <michele.ceresoli@polimi.it>
2024-01-06 16:03:45 +0100

    Updated tests

--- test/rotations.jl
+++ test/rotations.jl
@@ -1,21 +1,70 @@
+
+# Common routines
 r2a = 180 / π * 3600
 v2as = (x, y) -> acosd(max(-1, min(1, dot(x / norm(x), y / norm(y))))) * 3600
 
-@testset "Full Rotation without EOP" verbose=true begin 
-    for _ in 1:100 
-
-        tt_c = rand() 
-        tt_d = tt_c*Tempo.CENTURY2DAY
-        
-        v = rand(BigFloat, 3)
-        v /= norm(v)
-
-        # TODO: write these tests 
-
-    end
-end;
+# With that epoch lists, retrieve all the required EOP data 
+eopfile = "test/assets/eopc04_20.1972-now.txt"
+eop_parse_txt(iers2010a, eopfile, "test/assets/eopc04")
+eop_load_data!("test/assets/eopc04.eop.dat", iers2010a)
 
 @testset "Full Rotation with EOP" verbose=true begin 
     # TODO: write these tests 
+
+    @testset "CIP-motion" verbose=true begin 
+
+        # Retrieve the data
+        data = readdlm("test/assets/obspm-cipmotion.txt"; skipstart=2)
+        n = length(axes(data, 1))
+
+        for j = 1:n
+            row = data[j, :]
+
+            ep_utc = Epoch(
+                    "$(Int(row[2]))-$(Int(row[3]))-$(Int(row[4]))T"*
+                    "$(Int(row[5])):$(Int(row[6])):$(row[7]) UTC"
+            )
+
+            ep_tt = convert(TT, ep_utc)
+
+            R1 = DCM(row[8:16])'
+            R2 = iers_rot3_gcrf_to_cirf(j2000s(ep_tt), iers2010a)'
+
+            for _ in 1:10
+                v = rand(BigFloat, 3)
+                @test v2as(R1*v, R2*v) ≤ 3e-6
+            end
+
+        end
+
+    end
+
+    @testset "Polar-motion" verbose=true begin 
+
+        # Retrieve the data
+        data = readdlm("test/assets/obspm-pm.txt"; skipstart=2)
+        n = length(axes(data, 1))
+    
+        for j = 1:n
+            row = data[j, :]
+
+            ep_utc = Epoch(
+                    "$(Int(row[2]))-$(Int(row[3]))-$(Int(row[4]))T"*
+                    "$(Int(row[5])):$(Int(row[6])):$(row[7]) UTC"
+            )
+
+            ep_tt = convert(TT, ep_utc)
+
+            R1 = DCM(row[8:16])'
+            R2 = iers_rot3_itrf_to_tirf(j2000s(ep_tt), iers2010a)
+
+            for _ in 1:10
+                v = rand(BigFloat, 3)
+                @test v2as(R1*v, R2*v) ≤ 3e-6
+            end
+
+        end
+    
+    end
 
 end;
--- test/runtests.jl
+++ test/runtests.jl
@@ -32,17 +32,6 @@ end;
     include("polar.jl")
     include("era.jl")
 
-    include("rotations.jl")
+    # include("rotations.jl")
 
 end;
-
-tt_c = rand()/4
-tt_d = tt_c*Tempo.CENTURY2DAY
-
-ut1_d = tt_d + IERSConventions.offset_tt2ut1(tt_d*Tempo.DAY2SEC)/Tempo.DAY2SEC
-
-# --- Testing 1996 model (< 10 μas)
-# GMST 
-gm  = iers_gmst(iers1996, tt_c)
-gme = gmst82(DJ2000, ut1_d)
-@test r2a*abs(gm-gme) ≤ 1e-5
