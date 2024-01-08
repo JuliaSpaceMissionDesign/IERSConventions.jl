22560962ecf93182f613652e091cdb10c9c63ef6
MicheleCeresoli <michele.ceresoli@polimi.it>
2024-01-08 22:50:37 +0100

    Added tests on approximations

--- test/rotations.jl
+++ test/rotations.jl
@@ -12,166 +12,184 @@ eop_load_data!(joinpath(@__DIR__, "assets", "eopc04.eop.dat"), iers2010a)
 
 @testset "Rotations" verbose=true begin 
 
-    @testset "CIRF-to-GCRF" verbose=true begin 
+    @testset "CIO-based approach" verbose=true begin 
+        @testset "CIRF-to-GCRF" verbose=true begin 
 
-        # Retrieve the data
-        data = readdlm(joinpath(test_dir, "obspm-cipmotion.txt"); skipstart=2)
-        n = length(axes(data, 1))
+            # Retrieve the data
+            data = readdlm(joinpath(test_dir, "obspm-cipmotion.txt"); skipstart=2)
+            n = length(axes(data, 1))
 
-        for j = 1:n
-            row = data[j, :]
+            for j = 1:n
+                row = data[j, :]
 
-            ep_utc = Epoch(
-                    "$(Int(row[2]))-$(Int(row[3]))-$(Int(row[4]))T"*
-                    "$(Int(row[5])):$(Int(row[6])):$(row[7]) UTC"
-            )
+                ep_utc = Epoch(
+                        "$(Int(row[2]))-$(Int(row[3]))-$(Int(row[4]))T"*
+                        "$(Int(row[5])):$(Int(row[6])):$(row[7]) UTC"
+                )
 
-            ep_tt = convert(TT, ep_utc)
+                ep_tt = convert(TT, ep_utc)
 
-            # The transpose is needed to get the matrix in row-major! 
-            R1 = DCM(row[8:16])'
-            R2 = iers_rot3_gcrf_to_cirf(j2000s(ep_tt), iers2010a)'
+                # The transpose is needed to get the matrix in row-major! 
+                R1 = DCM(row[8:16])'
+                R2 = iers_rot3_gcrf_to_cirf(j2000s(ep_tt), iers2010a)'
 
-            for _ in 1:10
-                v = rand(BigFloat, 3)
-                @test v2as(R1*v, R2*v) ≤ 3e-6
+                for _ in 1:10
+                    v = rand(BigFloat, 3)
+                    @test v2as(R1*v, R2*v) ≤ 3e-6
+                end
+
             end
 
         end
 
-    end
+        @testset "TIRF-to-CIRF without ΔUT1" verbose=true begin 
+            data = readdlm(joinpath(test_dir, "obspm-era-nodut1.txt"); skipstart=2)
+            n = length(axes(data, 1))
 
-    @testset "TIRF-to-CIRF without ΔUT1" verbose=true begin 
-        data = readdlm(joinpath(test_dir, "obspm-era-nodut1.txt"); skipstart=2)
-        n = length(axes(data, 1))
+            for j = 1:n 
+                row = data[j, :]
 
-        for j = 1:n 
-            row = data[j, :]
+                ep_utc = Epoch(
+                        "$(Int(row[2]))-$(Int(row[3]))-$(Int(row[4]))T"*
+                        "$(Int(row[5])):$(Int(row[6])):$(row[7]) UTC"
+                )
 
-            ep_utc = Epoch(
-                    "$(Int(row[2]))-$(Int(row[3]))-$(Int(row[4]))T"*
-                    "$(Int(row[5])):$(Int(row[6])):$(row[7]) UTC"
-            )
+                ep_ut1 = convert(UT1, ep_utc)
 
-            ep_ut1 = convert(UT1, ep_utc)
+                R1 = DCM(row[8:16])'
+                R2 = iers_era_rotm(iers2010a, j2000(ep_utc))'
 
-            R1 = DCM(row[8:16])'
-            R2 = iers_era_rotm(iers2010a, j2000(ep_utc))'
-
-            for _ in 1:10
-                v = rand(BigFloat, 3)
-                @test v2as(R1*v, R2*v) ≤ 1e-6 
-            end
-        end
-    end
-
-    @testset "TIRF-to-CIRF with ΔUT1" verbose=true begin 
-
-        # Retrieve the data 
-        data = readdlm(joinpath(test_dir, "obspm-era.txt"); skipstart=2)
-        n = length(axes(data, 1))
-
-        for j = 1:n 
-            row = data[j, :]
-
-            ep_utc = Epoch(
-                    "$(Int(row[2]))-$(Int(row[3]))-$(Int(row[4]))T"*
-                    "$(Int(row[5])):$(Int(row[6])):$(row[7]) UTC"
-            )
-
-            ep_ut1 = convert(UT1, ep_utc)
-
-            R1 = DCM(row[8:16])'
-            R2 = iers_era_rotm(iers2010a, j2000(ep_ut1))'
-
-            for _ in 1:10
-                v = rand(BigFloat, 3)
-                @test v2as(R1*v, R2*v) ≤ 20e-6 
+                for _ in 1:10
+                    v = rand(BigFloat, 3)
+                    @test v2as(R1*v, R2*v) ≤ 1e-6 
+                end
             end
         end
 
-    end
+        @testset "TIRF-to-CIRF with ΔUT1" verbose=true begin 
 
-    @testset "ITRF-to-TIRF" verbose=true begin 
+            # Retrieve the data 
+            data = readdlm(joinpath(test_dir, "obspm-era.txt"); skipstart=2)
+            n = length(axes(data, 1))
 
-        # Retrieve the data
-        data = readdlm(joinpath(test_dir, "obspm-pm.txt"); skipstart=2)
-        n = length(axes(data, 1))
-    
-        for j = 1:n
-            row = data[j, :]
+            for j = 1:n 
+                row = data[j, :]
 
-            ep_utc = Epoch(
-                    "$(Int(row[2]))-$(Int(row[3]))-$(Int(row[4]))T"*
-                    "$(Int(row[5])):$(Int(row[6])):$(row[7]) UTC"
-            )
+                ep_utc = Epoch(
+                        "$(Int(row[2]))-$(Int(row[3]))-$(Int(row[4]))T"*
+                        "$(Int(row[5])):$(Int(row[6])):$(row[7]) UTC"
+                )
 
-            ep_tt = convert(TT, ep_utc)
+                ep_ut1 = convert(UT1, ep_utc)
 
-            R1 = DCM(row[8:16])'
-            R2 = iers_rot3_itrf_to_tirf(j2000s(ep_tt), iers2010a)
+                R1 = DCM(row[8:16])'
+                R2 = iers_era_rotm(iers2010a, j2000(ep_ut1))'
 
-            for _ in 1:10
-                v = rand(BigFloat, 3)
-                @test v2as(R1*v, R2*v) ≤ 3e-6
+                for _ in 1:10
+                    v = rand(BigFloat, 3)
+                    @test v2as(R1*v, R2*v) ≤ 20e-6 
+                end
             end
 
         end
-    
-    end
 
-    @testset "ITRF-to-CIRF" verbose=true begin 
+        @testset "ITRF-to-TIRF" verbose=true begin 
 
-        # Retrieve the data
-        data = readdlm(joinpath(test_dir, "obspm-erapm.txt"); skipstart=2)
-        n = length(axes(data, 1))
-    
-        for j = 1:n
-            row = data[j, :]
+            # Retrieve the data
+            data = readdlm(joinpath(test_dir, "obspm-pm.txt"); skipstart=2)
+            n = length(axes(data, 1))
+        
+            for j = 1:n
+                row = data[j, :]
 
-            ep_utc = Epoch(
-                    "$(Int(row[2]))-$(Int(row[3]))-$(Int(row[4]))T"*
-                    "$(Int(row[5])):$(Int(row[6])):$(row[7]) UTC"
-            )
+                ep_utc = Epoch(
+                        "$(Int(row[2]))-$(Int(row[3]))-$(Int(row[4]))T"*
+                        "$(Int(row[5])):$(Int(row[6])):$(row[7]) UTC"
+                )
 
-            ep_tt = convert(TT, ep_utc)
+                ep_tt = convert(TT, ep_utc)
 
-            R1 = DCM(row[8:16])'
-            R2 = iers_rot3_itrf_to_cirf(j2000s(ep_tt), iers2010a)
+                R1 = DCM(row[8:16])'
+                R2 = iers_rot3_itrf_to_tirf(j2000s(ep_tt), iers2010a)
 
-            for _ in 1:10
-                v = rand(BigFloat, 3)
-                @test v2as(R1*v, R2*v) ≤ 20e-6
+                for _ in 1:10
+                    v = rand(BigFloat, 3)
+                    @test v2as(R1*v, R2*v) ≤ 3e-6
+                end
+
+            end
+        
+        end
+
+        @testset "ITRF-to-CIRF" verbose=true begin 
+
+            # Retrieve the data
+            data = readdlm(joinpath(test_dir, "obspm-erapm.txt"); skipstart=2)
+            n = length(axes(data, 1))
+        
+            for j = 1:n
+                row = data[j, :]
+
+                ep_utc = Epoch(
+                        "$(Int(row[2]))-$(Int(row[3]))-$(Int(row[4]))T"*
+                        "$(Int(row[5])):$(Int(row[6])):$(row[7]) UTC"
+                )
+
+                ep_tt = convert(TT, ep_utc)
+
+                R1 = DCM(row[8:16])'
+                R2 = iers_rot3_itrf_to_cirf(j2000s(ep_tt), iers2010a)
+
+                for _ in 1:10
+                    v = rand(BigFloat, 3)
+                    @test v2as(R1*v, R2*v) ≤ 20e-6
+                end
+
+            end
+        
+        end
+
+        @testset "GCRF-to-ITRF" verbose=true begin 
+            # Retrieve the data
+            data = readdlm(joinpath(test_dir, "obspm-full.txt"); skipstart=2)
+            n = length(axes(data, 1))
+        
+            for j = 1:n
+                row = data[j, :]
+
+                ep_utc = Epoch(
+                        "$(Int(row[2]))-$(Int(row[3]))-$(Int(row[4]))T"*
+                        "$(Int(row[5])):$(Int(row[6])):$(row[7]) UTC"
+                )
+
+                ep_tt = convert(TT, ep_utc)
+
+                R  = DCM(row[8:16])'
+                Ra = iers_rot3_gcrf_to_itrf(j2000s(ep_tt), iers2010a)'
+
+                # Once we guarantee that the IAU 2006/2000 model is correct, we test all the 
+                # other 2010 approximations against that (within their accuracy)
+                Rb = iers_rot3_gcrf_to_itrf(j2000s(ep_tt), iers2010b)'
+                Rc = iers_rot3_gcrf_to_itrf(j2000s(ep_tt), CPNc)'
+                Rd = iers_rot3_gcrf_to_itrf(j2000s(ep_tt), CPNd)' 
+
+                for _ in 1:10
+                    v = rand(BigFloat, 3)
+                    @test v2as(R*v, Ra*v) ≤ 20e-6
+                    @test v2as(Ra*v, Rb*v) ≤ 3e-3
+                    @test v2as(Ra*v, Rc*v) ≤ 50e-3
+                    @test v2as(Ra*v, Rd*v) ≤ 2
+
+                end
+
             end
 
         end
-    
-    end
 
-    @testset "GCRF-to-ITRF" verbose=true begin 
-        # Retrieve the data
-        data = readdlm(joinpath(test_dir, "obspm-full.txt"); skipstart=2)
-        n = length(axes(data, 1))
-    
-        for j = 1:n
-            row = data[j, :]
+    end 
 
-            ep_utc = Epoch(
-                    "$(Int(row[2]))-$(Int(row[3]))-$(Int(row[4]))T"*
-                    "$(Int(row[5])):$(Int(row[6])):$(row[7]) UTC"
-            )
+    @testset "Equinox-based approach" verbose=true begin 
 
-            ep_tt = convert(TT, ep_utc)
-
-            R1 = DCM(row[8:16])'
-            R2 = iers_rot3_gcrf_to_itrf(j2000s(ep_tt), iers2010a)'
-
-            for _ in 1:10
-                v = rand(BigFloat, 3)
-                @test v2as(R1*v, R2*v) ≤ 20e-6
-            end
-
-        end
 
     end
 
